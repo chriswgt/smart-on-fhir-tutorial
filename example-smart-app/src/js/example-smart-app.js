c83bd06859f7e15603be3d6ab4a9a06a3b298ed3
Chris <chrismwalsh@outlook.com>
2019-10-23 14:29:05 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -15,7 +15,7 @@
                     type: 'Observation',
                     query: {
                       code: {
-                        $or: ['http://loinc.org|3141-9', 'http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                        $or: ['http://loinc.org|29463-7', 'http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
                       }
@@ -36,7 +36,7 @@
             fname = patient.name[0].given.join(' ');
             lname = patient.name[0].family.join(' ');
           }
-          var weight = byCodes('3141-9');
+          var weight = byCodes('29463-7');
           var height = byCodes('8302-2');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
